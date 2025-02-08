e9087be37c860c9df8b7cbb6fa6638da67e9a99a
nathan <nathan.esslinger67@gmail.com>
2025-02-08 11:14:03 +0100

    implementing HTTP Server response

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,44 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Server is starting...");
 
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+try
+{
+    var socket = server.AcceptSocket();
+    try
+    {
+        await socket.SendAsync(Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+    finally
+    {
+        if (socket.Connected)
+        {
+            Console.WriteLine("ShutingDown the connection");
+            socket.Shutdown(SocketShutdown.Both);
+        }
+
+        Console.WriteLine("Closing the connection");
+        socket.Close();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+finally
+{
+    server.Stop();
+}
+
+
+

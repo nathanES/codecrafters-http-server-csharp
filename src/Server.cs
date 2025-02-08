5d0c798d68402e3344125bb1a32f7ec6e36a7a34
nathan <nathan.esslinger67@gmail.com>
2025-02-08 11:19:52 +0100

    implementing HTTP Server response

--- src/Server.cs
+++ src/Server.cs
@@ -2,43 +2,21 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Server is starting...");
-
+Console.WriteLine("Starting the server");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-try
+Console.WriteLine("Server started");
+var socket = server.AcceptSocket();
+socket.Send(Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+if (socket.Connected)
 {
-    var socket = server.AcceptSocket();
-    try
-    {
-        await socket.SendAsync(Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-        throw;
-    }
-    finally
-    {
-        if (socket.Connected)
-        {
-            Console.WriteLine("ShutingDown the connection");
-            socket.Shutdown(SocketShutdown.Both);
-        }
-
-        Console.WriteLine("Closing the connection");
-        socket.Close();
-    }
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    throw;
-}
-finally
-{
-    server.Stop();
+    Console.WriteLine("ShutingDown the connection");
+    socket.Shutdown(SocketShutdown.Both);
 }
 
-
-
+Console.WriteLine("Closing the connection");
+socket.Close();
+Console.WriteLine("Connection closed");
+Console.WriteLine("Stopping the server");
+server.Stop();
+Console.WriteLine("Server stopped");

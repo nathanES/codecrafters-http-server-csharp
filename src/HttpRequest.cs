7155e828b86d472f3bb786cc36d288e91597cc86
nathan <nathan.esslinger67@gmail.com>
2025-02-09 10:37:41 +0100

    Read Header

--- src/HttpRequest.cs
+++ src/HttpRequest.cs
@@ -16,6 +16,7 @@ public class HttpRequest
 
     private static HttpRequest? Parse(string httpRequest)
     {
+        var httpRequestBuilder = new HttpRequestBuilder();
         var httpRequestParts = httpRequest.Split("\r\n");
         if(httpRequestParts.Length < 1)
             return null;
@@ -23,33 +24,52 @@ public class HttpRequest
         var httpRequestLine = httpRequestParts[0].Split(" ");
         if(httpRequestLine.Length < 3)
             return null;
-              
-        return new HttpRequestBuilder()
+
+        httpRequestBuilder
             .SetHttpMethod(httpRequestLine[0])
             .SetRequestTarget(httpRequestLine[1])
-            .SetHttpVersion(httpRequestLine[2])
+            .SetHttpVersion(httpRequestLine[2]);
+        
+        foreach (var header in httpRequestParts[1..^1])
+        {
+            string[] headerParts = header.Split(":");
+            if(headerParts.Length < 2)
+                continue;
+            httpRequestBuilder.SetHeader(headerParts[0].Trim(), headerParts[1].Trim());
+        } 
+        
+        return httpRequestBuilder
             .Build();
     }
         
     public class HttpRequestBuilder
     {
-        private readonly HttpRequest? _httpRequest = new();
+        private readonly HttpRequest _httpRequest = new();
 
         public HttpRequestBuilder SetHttpMethod(string httpMethod)
         {
-            _httpRequest!.HttpMethod = httpMethod;
+            _httpRequest.HttpMethod = httpMethod;
             return this;
         }
         public HttpRequestBuilder SetRequestTarget(string requestTarget)
         {
-            _httpRequest!.RequestTarget = requestTarget;
+            _httpRequest.RequestTarget = requestTarget;
             return this;
         } 
         public HttpRequestBuilder SetHttpVersion(string httpVersion)
         {
-            _httpRequest!.HttpVersion = httpVersion;
+            _httpRequest.HttpVersion = httpVersion;
             return this;
         } 
+        public HttpRequestBuilder SetHeader(string name, string value)
+        {
+            if (_httpRequest.Headers.ContainsKey(name))
+                _httpRequest.Headers[name] = value;
+            else
+                _httpRequest.Headers.Add(name, value);
+            return this;
+        }
+
         public HttpRequest? Build()
         {
             return _httpRequest;

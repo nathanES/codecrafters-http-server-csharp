ff30a6d29acdfa46cc25b00f893bf18b550227c5
nathan <nathan.esslinger67@gmail.com>
2025-02-11 19:24:14 +0100

    Adding compression

--- src/Utils/Encoding/EncodingHelper.cs
+++ src/Utils/Encoding/EncodingHelper.cs
@@ -23,7 +23,7 @@ public static class EncodingHelper
     private static byte[] GzipCompress(byte[] bodyBytes)
     {
         using var memoryStream = new MemoryStream();
-        using var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress);
+        using var gZipStream = new GZipStream(memoryStream, CompressionLevel.Optimal);
         gZipStream.Write(bodyBytes, 0, bodyBytes.Length);
         return memoryStream.ToArray();
     }
